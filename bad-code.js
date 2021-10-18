634ffb43b7ddce7d7d153bda197f914803fd3a03
Janani Wasana <56547353+Jwasana@users.noreply.github.com>
2021-10-19 01:03:05 +0530

    using esLint

---
+++ bad-code.js
@@ -0,0 +1,43 @@
+/* globals ga twttr  */
+/* eslint-disable no-extend-native */
+
+const weather = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve({ temp: 29, conditions: 'Sunny with Clouds' });
+  }, 2000);
+});
+
+const tweets = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(['I like cake', 'BBQ is good too!']);
+  }, 500);
+});
+
+Promise
+  .all([weather, tweets])
+  .then((responses) => {
+    const [weatherInfo, tweetInfo] = responses;
+    console.log(weatherInfo, tweetInfo);
+  });
+
+const postsPromise = fetch('http://wesbos.com/wp-json/wp/v2/posts');
+const streetCarsPromise = fetch('http://data.ratp.fr/api/datasets/1.0/search/?q=paris');
+
+Promise
+  .all([postsPromise, streetCarsPromise])
+  .then((responses) => Promise.all(responses.map((res) => res.json())))
+  .then((responses) => {
+    console.log(responses);
+  });
+
+ga.track();
+twttr.track();
+
+/*eslint-disable*/
+let arr = ['a', 'b', 'c']
+
+arr.includes('a', -100) // true
+arr.includes('b', -100) // true
+arr.includes('c', -100) // true
+arr.includes('a', -2)   // false
+  
